30308ccf0e20c6db957320ffdc0a908395f87418
aprillialee <aprilliapmlee@gmail.com>
2020-10-27 17:08:04 +0000

    created NavBar with responsiveness

---
+++ components/primitives/NavBar/MenuOptions.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+function MenuOption({open}) {
+  return (
+<MenuList open={open}>
+  <li>NEWS</li>
+  <li>TUTORIALS</li>
+  <li>TOOLS</li>
+  <li>INSPIRATION</li>
+</MenuList>
+  )
+}
+
+const MenuList = styled.ul`
+list-style: none;
+display: flex;
+flex-flow: row nowrap;
+
+li{
+border-left: 0.5px solid #AFAFAF;
+padding-left: 25px;
+padding-right: 25px;
+font-size: 14px;
+height: 20px;
+align-self: center;
+font-weight: 400;
+}
+
+
+@media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #686c82;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    margin-top: 0px;
+    right: 0;
+    height: 100%;
+    width: 300px;
+    transition: transform 0.3s ease-in-out;
+    padding-top: 2.5rem;
+  
+
+    li {
+      border: none;
+      font-size: 15px;
+      padding: 20px 10px;
+      color: white;
+    }
+}
+`
+export default MenuOption;
---
+++ components/primitives/NavBar/NavBar.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+import MenuOption from "./MenuOptions"
+import Burger from "./Burger";
+
+function NavBar() {
+  return(
+    <NavBarStyled>
+      <Logo img src="./3c.png"/>
+      <Gap/>
+      <Search placeholder="SEARCH..."/>
+      <Burger/>
+    </NavBarStyled>
+
+  )
+}
+
+const NavBarStyled = styled.div`
+width: 100%;
+height: 60px;
+background: #FFFFFF;
+box-shadow: 0.5px 0.5px 2.5px;
+display: flex;
+flex-direction: row;
+
+`
+
+const Gap = styled.div`
+width: 50px;
+height: 100%;
+@media (max-width: 768px){
+  display: none;
+}
+`
+const Logo = styled.img`
+height: 40px;
+width: 40px;
+margin: 10px;
+margin-left: 80px;
+
+@media (max-width: 768px) {
+  margin: 10px;
+  padding: 0;
+}
+`
+
+const Search = styled.input`
+margin-left: 35px;
+width: 40%;
+height: 60%;
+margin-top: 12px;
+border: 1px solid #686c82;
+border-radius: 6px;
+padding: 10px;
+@media (max-width: 768px) {
+  width: 60%;
+  margin-left: 5px;
+}
+
+@media (min-width: 1024px) {
+margin-left: 10px;
+width: 30%;
+}
+`
+
+export default NavBar;
